1783642770552f8383947435dd0b7acf0697a3f4
Fwolf <fwolf.aide+git@gmail.com>
2014-02-19 16:45:50 +0800

    New class Imap

---
+++ ImapDeleteForMh/Imap.php
@@ -0,0 +1,177 @@
+<?php
+namespace Fwolf\Bin\ImapDeleteForMh;
+
+
+/**
+ * IMAP operator
+ *
+ * @copyright   Copyright 2014 Fwolf
+ * @author      Fwolf <fwolf.aide+imap-del-for-mh.php@gmail.com>
+ * @license     http://opensource.org/licenses/mit-license MIT
+ * @since       2014-02-19
+ */
+class Imap
+{
+    /**
+     * IMAP connection
+     *
+     * @var resource
+     */
+    protected $connection = null;
+
+
+    /**
+     * Constructor
+     *
+     * @param   string  $host
+     * @param   string  $user
+     * @param   string  $pass
+     */
+    public function __construct($host, $user, $pass)
+    {
+        $this->connection = $this->connect($host, $user, $pass);
+    }
+
+
+    /**
+     * Connect to IMAP server
+     *
+     * @param   string  $host
+     * @param   string  $user
+     * @param   string  $pass
+     * @return  resource
+     */
+    protected function connect($host, $user, $pass)
+    {
+        try {
+            $connection = imap_open($host, $user, $pass);
+
+            return $connection;
+
+        } catch (\Exception $e) {
+            throw new \Exception(
+                'IMAP server connect error: ' . $e->getMessage()
+            );
+        }
+    }
+
+
+    /**
+     * Delete a message
+     *
+     * @param   int     $uid
+     * @param   string  $trashDir   IMAP folder
+     * @return  boolean
+     */
+    public function delete($uid, $trashDir)
+    {
+        try {
+            $op1 = imap_mail_move(
+                $this->connection,
+                $uid,
+                $trashDir,
+                CP_UID
+            );
+
+            $op2 = imap_delete($this->connection, $uid, FT_UID);
+
+            if ($op1 && $op2) {
+                imap_expunge($this->connection);
+            }
+
+            return true;
+
+        } catch (\Exception $e) {
+            throw new \Exception(
+                'IMAP delete error: ' . $e->getMessage()
+            );
+        }
+    }
+
+
+    /**
+     * Search for specified message, return message uid
+     *
+     * Content of $config:
+     * {date, from, received(optional), messageId}
+     *
+     * @param   array   $config
+     * @return  int
+     */
+    public function search(array $config)
+    {
+        $result = $this->searchByDateAndFrom(
+            $config['date'],
+            $config['from']
+        );
+
+        if (empty($result) && isset($config['received'])) {
+            $result = $this->searchByReceived($config['received']);
+        }
+
+        if (empty($result)) {
+            // Not found
+            return -1;
+        }
+
+        $mailOverview = imap_fetch_overview(
+            $this->connection,
+            implode(',', $result),
+            FT_UID
+        );
+
+        if (1 == count($mailOverview)) {
+            return $mailOverview[0]->uid;
+
+        } else {
+            // Got multiple result, match with messageId
+            foreach ((array)$mailOverview as $mail) {
+                if ($mail->message_id == $config['messageId']) {
+                    return $mail->uid;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+
+    /**
+     * @param   string  $date
+     * @param   string  $from
+     * @return  array
+     */
+    protected function searchByDateAndFrom($date, $from)
+    {
+        $dateSince = date('d-M-Y', strtotime($date . '-1 day'));
+        $dateBefore = date('d-M-Y', strtotime($date . '+1 day'));
+
+        $condition = "SINCE '$dateSince' BEFORE '$dateBefore'";
+
+        if (1 === preg_match('/<(.+)>/', $from, $match)) {
+            $from = $match[1];
+        }
+        $from = addslashes($from);
+
+        if (false !== strpos($from, '@')) {
+            $condition .= " From '$from'";
+        }
+
+        return imap_search($this->connection, $condition, SE_UID);
+    }
+
+
+    /**
+     * @param   string  $received
+     * @return  array
+     */
+    protected function searchByReceived($received)
+    {
+        $receivedSince = date('d-M-Y', strtotime($received . '-1 day'));
+        $receivedBefore = date('d-M-Y', strtotime($received . '+1 day'));
+
+        $condition = "SINCE '$receivedSince' BEFORE '$receivedBefore'";
+
+        return imap_search($this->connection, $condition, SE_UID);
+    }
+}
---
+++ ImapDeleteForMh/Test/ImapTest.php
@@ -0,0 +1,203 @@
+<?php
+namespace Fwolf\Bin\ImapDeleteForMh\Test;
+
+use Fwlib\Bridge\PHPUnitTestCase;
+use Fwolf\Bin\ImapDeleteForMh\Imap;
+
+/**
+ * @copyright   Copyright 2014 Fwolf
+ * @author      Fwolf <fwolf.aide+imap-del-for-mh.php@gmail.com>
+ * @license     http://opensource.org/licenses/mit-license MIT
+ * @since       2014-02-19
+ */
+class ImapTest extends PHPUnitTestCase
+{
+    public static $dummyMessageId = 'dummy message id';
+    protected $dummyResource = 'dummy imap connection';
+    public static $imap_delete = null;
+    public static $imap_fetch_overview = array();
+    public static $imap_open = null;
+    public static $imap_search = array();
+    public static $imap_search_condition = '';
+
+
+    protected function buildMock()
+    {
+        $imap = $this->getMock(
+            '\Fwolf\Bin\ImapDeleteForMh\Imap',
+            null,
+            array('host', 'user', 'pass')
+        );
+
+        return $imap;
+    }
+
+
+    public function testConnect()
+    {
+        self::$imap_open = $this->dummyResource;
+        $imap = $this->buildmock();
+
+        $this->assertequals(
+            $this->dummyResource,
+            $this->reflectionget($imap, 'connection')
+        );
+    }
+
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage connect error
+     */
+    public function testConnectWithException()
+    {
+        self::$imap_open = null;
+        $imap = $this->buildmock();
+    }
+
+
+    public function testDelete()
+    {
+        self::$imap_open = $this->dummyResource;
+        self::$imap_delete = 'successful';
+        $imap = $this->buildmock();
+
+        $this->assertTrue($imap->delete(42, 'trash/'));
+    }
+
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage delete error
+     */
+    public function testDeleteWithException()
+    {
+        self::$imap_open = $this->dummyResource;
+        self::$imap_delete = null;
+        $imap = $this->buildmock();
+
+        $this->assertTrue($imap->delete(42, 'trash/'));
+    }
+
+
+    public function testSearch()
+    {
+        self::$imap_open = $this->dummyResource;
+        $imap = $this->buildmock();
+
+        // Search by date and from, with empty result
+        self::$imap_search = array();
+        $config = array(
+            'date'  => '2014-02-19',
+            'from'  => '发信人 <sender@domain.tld>',
+            'messageId' => 'dummy message id',
+        );
+
+        $this->assertEquals(-1, $imap->search($config));
+        $this->assertEquals(
+            "SINCE '18-Feb-2014' BEFORE '20-Feb-2014' From 'sender@domain.tld'",
+            self::$imap_search_condition
+        );
+
+
+        // Search by received, with empty result
+        $config['received'] = '2014-02-20';
+
+        $this->assertEquals(-1, $imap->search($config));
+        $this->assertEquals(
+            "SINCE '19-Feb-2014' BEFORE '21-Feb-2014'",
+            self::$imap_search_condition
+        );
+
+
+        // Search with empty result
+        self::$imap_search = array();
+        $this->assertEquals(-1, $imap->search($config));
+
+
+        // Search with single result
+        self::$imap_search = array(42);
+        $this->assertEquals(42, $imap->search($config));
+
+
+        // Search with multiple result, use message id filter too
+        self::$imap_search = array(42, 1);
+        $config['messageId'] = self::$dummyMessageId;
+        $this->assertEquals(1, $imap->search($config));
+
+
+        // Search with multiple result, message id filter to empty
+        self::$imap_search = array(42, 1);
+        $config['messageId'] = self::$dummyMessageId . 'not exists';
+        $this->assertEquals(-1, $imap->search($config));
+    }
+}
+
+
+/* Mock imap function */
+namespace Fwolf\Bin\ImapDeleteForMh;
+
+
+function imap_delete($imaapStream, $uid, $option)
+{
+    if (is_null(\Fwolf\Bin\ImapDeleteForMh\Test\ImapTest::$imap_delete)) {
+        throw new \Exception('imap delete fail');
+
+    } else {
+        return true;
+    }
+}
+
+
+function imap_expunge($imapStream)
+{
+    return true;
+}
+
+
+function imap_fetch_overview($imapStream, $sequence, $option)
+{
+    $mailOverview = array();
+
+    foreach (explode(',', $sequence) as $uid) {
+        $mail = new \stdClass;
+
+        $mail->uid = $uid;
+        if (1 == $uid) {
+            $mail->message_id = \Fwolf\Bin\ImapDeleteForMh\Test\ImapTest::
+                $dummyMessageId;
+        } else {
+            $mail->message_id = $uid;
+        }
+
+        $mailOverview[] = $mail;
+    }
+
+    return $mailOverview;
+}
+
+
+function imap_mail_move($imapStream, $uid, $mailbox, $option)
+{
+    return true;
+}
+
+
+function imap_open($host, $user, $pass)
+{
+    if (is_null(\Fwolf\Bin\ImapDeleteForMh\Test\ImapTest::$imap_open)) {
+        throw new \Exception('imap connect fail');
+
+    } else {
+        return \Fwolf\Bin\ImapDeleteForMh\Test\ImapTest::$imap_open;
+    }
+}
+
+
+function imap_search($imapStream, $condition, $option)
+{
+    \Fwolf\Bin\ImapDeleteForMh\Test\ImapTest::$imap_search_condition =
+        $condition;
+
+    return \Fwolf\Bin\ImapDeleteForMh\Test\ImapTest::$imap_search;
+}
